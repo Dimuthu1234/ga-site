8290565b7a24fbe559bea1f62aecca11f35dd5fc
d.jayalath@stylesoftware.co <d.jayalath@stylesoftware.co>
2018-09-13 11:01:02 +0530

    testo

--- resources/views/ourTestimonials.blade.php
+++ resources/views/ourTestimonials.blade.php
@@ -648,7 +648,7 @@
 
 
                                                 {{--<div class="elementor-background-overlay"></div>--}}
-                                                <div class="elementor-container elementor-column-gap-default" style="margin-bottom: 10%;margin-top: 8%">
+                                                <div class="elementor-container elementor-column-gap-default" style="margin-bottom: 15%;margin-top: 5%">
                                                     <div class="elementor-row">
 
                                                         {{--<div class="col-md-12">--}}
